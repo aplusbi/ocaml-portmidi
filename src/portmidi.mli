675f8e142b76e8dc5581e748e96a745eece136fb
Niki Yoshiuchi <aplusbi@gmail.com>
2011-03-28 22:58:52 -0400

    Added interface file and some documentation

---
+++ src/portmidi.mli
@@ -0,0 +1,116 @@
+(**
+* Bindings for the portmidi library.
+*
+* @author Niki Yoshiuchi
+*)
+
+(** {2 General} *)
+(** Initialize the portmidi library.  Call this before opening any streams *)
+external init : unit -> unit = "caml_pm_initialize"
+
+(** Close the library *)
+external terminate : unit -> unit = "caml_pm_terminate"
+
+(** {2 Devices} *)
+
+(** Device information type *)
+type device_info = {
+  struct_version : int;
+  interf : string;
+  name : string;
+  input : bool;
+  output : bool;
+  opened : bool;
+}
+
+(** [count_devices ()] returns the number of MIDI devices available *)
+external count_devices : unit -> int = "caml_pm_count_devices"
+
+(** [get_default_input_device_id ()] returns the default input MIDI device *)
+external get_default_input_device_id : unit -> int
+  = "caml_pm_get_default_input_device_id"
+  
+(** [get_default_output_device_id ()] returns the default output MIDI device *)
+external get_default_output_device_id : unit -> int
+  = "caml_pm_get_default_output_device_id"
+
+(** [get_device_info id] returns a [device_info] record for device [id] *)
+external get_device_info : int -> device_info = "caml_pm_get_device_info"
+
+(** {2 Streams} *)
+
+(** Stream type *)
+type stream
+
+(** [open_input ?time_proc:(Some time_fun) ?time_info:(Some time_arg) device_id buffer_size]
+ * opens a MIDI stream ready for input on device [device_id] with a buffer of
+ * [buffer_size].  [time_fun] is a function that returns a time stamp in
+ * milliseconds, [time_args] is the argument passed to [time_fun].  If they are
+ * set to [None] then the default time function is used. *)
+val open_input :
+  ?time_proc:('a -> int) option ->
+  ?time_info:'a option -> int -> int -> stream
+
+(** [open_input ?time_proc:(Some time_fun) ?time_info:(Some time_arg) device_id * buffer_size latency]
+ * opens a MIDI stream ready for ouput on device [device_id] with a buffer of
+ * [buffer_size].  [latency] is the delay in milliseconds applied to the time
+ * stamp. *)
+val open_output :
+  ?time_proc:('a -> int) option ->
+  ?time_info:'a option -> int -> int -> int -> stream
+
+(** Abort a stream *)
+external abort : stream -> unit = "caml_pm_abort"
+
+(** Close a stream *)
+external close : stream -> unit = "caml_pm_close"
+
+(** {2 Events} *)
+
+(** Event type *)
+type event = { message : Int32.t; timestamp : Int32.t; }
+
+(** [message status data1 data2] creates a message *)
+val message : int -> int -> int -> int32
+
+(** [message_status msg] returns the status of message [msg] *)
+val message_status : int32 -> int
+
+(** [message_data1 msg] returns the first data byte of message [msg] *)
+val message_data1 : int32 -> int
+
+(** [message_data2 msg] returns the second data byte of message [msg] *)
+val message_data2 : int32 -> int
+
+(** [message_contents msg] returns the status and data bytes of message [msg] *)
+val message_contents : int32 -> int * int * int
+
+(** [poll st] returns true of the input stream [st] has any events *)
+external poll : stream -> bool = "caml_pm_poll"
+
+(** [read_stream st events ofs len] reads up to [len] events from stream [st]
+ * and stores them in [events] starting at [ofs].  The return value is the
+ * actual number of events read. *)
+external read_stream : stream -> event array -> int -> int -> int = "caml_pm_read"
+
+(** [write_stream st events ofs len] writes [len] events to the stream [st] from
+ * [events] starting at [ofs]. *)
+external write_stream : stream -> event array -> int -> int -> unit
+  = "caml_pm_write"
+
+(** [write_short st when msg] writes the event formed by message [msg] and
+* timestamp [when] to stream [st] *)
+external write_short : stream -> Int32.t -> Int32.t -> unit
+  = "caml_pm_write_short"
+
+(** [write_short st when msg] writes the SysEx formed by message [msg] and
+* timestamp [when] to stream [st] *)
+external write_sysex : stream -> Int32.t -> string -> unit
+  = "caml_pm_write_sysex"
+
+module Time :
+  sig
+    external start : int -> unit = "caml_pt_start"
+    external stop : unit -> unit = "caml_pt_stop"
+    external time : unit -> Int32.t = "caml_pt_time"
+  end
